a9f82be4128e6326a3cbde63c7074f44eb6a37d9
wakki-0123 <s2131163ug@s.chibakoudai.jp>
2024-08-29 14:01:26 +0900

    eighth commit Raw_Transistors

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_wakki-0123_Raw_Transistors (
+module tt_um_wakki_0123_Raw_Transistors (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
